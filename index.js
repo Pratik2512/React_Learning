d1948459aa8359d0342d1e943405fa4840fba34b
Pratik Mohite <79019830+Pratik2512@users.noreply.github.com>
2023-03-06 00:38:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  App,
+  Index,
+  Name, 
+  Destr, 
+  Usestate, 
+  Usereducer, 
+  Useref, 
+  Form, 
+  Hook, 
+  Hookdata, 
+  Hookstate,
+  Api
+} from './App';
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App name="Jupiter"/>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/render" element={<App/>}/>
+      <Route path="/index" element={<Index/>}/>
+      <Route path="/" element={<Name name="Pratik"/>}>
+        <Route path="destr" element={<Destr name="Planet Earth"/>}/>
+      </Route>
+      <Route path="/usestate" element={<Usestate/>}/>
+      <Route path="/usereducer" element={<Usereducer/>}/>
+      <Route path="/useref" element={<Useref/>}>
+        <Route path="form" element={<Form/>}/>
+      </Route>
+      <Route path="/hook" element={<Hook/>}/>
+      <Route path="/hookdata" element={<Hookdata/>}/>
+      <Route path="/hookstates" element={<Hookstate/>}/>
+      <Route path="/graphqlapi" element={<Api/>}/>
+    </Routes>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
